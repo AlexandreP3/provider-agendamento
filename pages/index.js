6fb5cf47979cd407ede452de05ddbdad204f82e0
Alexandre Penha <138889297+AlexandreP3@users.noreply.github.com>
2026-03-12 12:57:45 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Consegui subir o arquivo do index e subir no git com comando amend= teste</h1>
+    return <h1>Patricia S2 Te amo</h1> 
+    <h2>Consegui subir o arquivo do index e subir no git com comando amend= teste</h2>
 }
 
 export default Home;
