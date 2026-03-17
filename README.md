d26ef8321d59d60d11763742bc41304fd87318d7
Alexandre Penha <138889297+AlexandreP3@users.noreply.github.com>
2026-03-17 13:49:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # provider-agendamento
+
 projeto-agendamento
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <><h1>Patricia S2 Te amo</h1><h2>Consegui subir o arquivo do index e subir no git com comando amend= teste</h2></>
+  return (
+    <>
+      <h1>Patricia S2 Te amo</h1>
+      <h2>
+        Consegui subir o arquivo do index e subir no git com comando amend=
+        teste
+      </h2>
+    </>
+  );
 }
 
 export default Home;
