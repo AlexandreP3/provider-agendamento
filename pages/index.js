040248b1cc4d0bd44483bbd71b0dfba829d5e03c
Alexandre Penha <138889297+AlexandreP3@users.noreply.github.com>
2026-06-08 23:42:19 +0000

    pagina

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Patricia S2 Te amo</h1>
+      <h1>Teste subiu com sucesso</h1>
       <h2>
         Consegui subir o arquivo do index e subir no git com comando amend=
         teste
